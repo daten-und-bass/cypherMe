ea16304e05ec52b37515acd5462a4dd875536d5c
arne-at-daten-und-bass <arne@daten-und-bass.io>
2016-12-03 15:37:59 +0100

    added viginere

--- public/app.js
+++ public/app.js
@@ -5,6 +5,7 @@ function cypherMe() {
   var cypherElement = document.getElementById('cypher');
   var cypher = cypherElement.options[cypherElement.selectedIndex];
   var shift = document.getElementById('shift').value;
+  var keyword = document.getElementById('keyword').value;
   var passwordEncrypted = document.getElementById('passwordEncrypted');
   var cypherUsed = document.getElementById('cypherUsed');
   var repeater = document.getElementById('repeater').value;
@@ -23,6 +24,8 @@ function cypherMe() {
       cypherUsed.value = cypher.text + ' (Repeater: ' + repeater + ')';
       break;
     case ('vigenere'):
+      passwordEncrypted.value = Array(1 + parseInt(repeater)).join(cypherMeViginereEncrypt(password, keyword));
+      cypherUsed.value = cypher.text + ' (Repeater: ' + repeater + ')';
       break;
     default:
       console.log('Default case');
@@ -36,6 +39,7 @@ function deCypherMe() {
   var cypherElementToDecrypt = document.getElementById('cypherToDecrypt');
   var cypherToDecrypt = cypherElementToDecrypt.options[cypherElementToDecrypt.selectedIndex];
   var shiftToDecrypt = document.getElementById('shiftToDecrypt').value;
+  var keywordToDecrypt = document.getElementById('keywordToDecrypt').value;
   var passwordDecrypted = document.getElementById('passwordDecrypted');
   var cypherUsedToDecrypt = document.getElementById('cypherUsedToDecrypt');
   var repeaterToDecrypt = document.getElementById('repeaterToDecrypt').value;
@@ -57,6 +61,8 @@ function deCypherMe() {
       cypherUsedToDecrypt.value = cypherToDecrypt.text + ' (Repeater: ' + repeaterToDecrypt + ')';
       break;
     case ('vigenere'):
+      passwordDecrypted.value = cypherMeViginereDecrypt(passwordToDecrypt.match(re)[0], keywordToDecrypt);
+      cypherUsedToDecrypt.value = cypherToDecrypt.text + ' (Repeater: ' + repeaterToDecrypt + ')';
       break;
     default:
       console.log('Default case');
@@ -77,7 +83,6 @@ function cypherMeCaesarDecrypt(passwordToDecrypt, shift) {
 
   return passwordDecrypted;
 }
-
 
 function cypherMeCaesar(password, shift) {
   var cypherCaesarPassword = '';
@@ -100,18 +105,18 @@ function cypherMeCaesar(password, shift) {
   return cypherCaesarPassword;
 }
 
-
 function cypherMeReverse(password) {
-  var passwordEnOrDecrypted = "";
+  var passwordEnOrDecrypted = '';
 
   for (var i = password.length - 1; i >= 0; i--) {
       passwordEnOrDecrypted += password[i];
   }
+
   return passwordEnOrDecrypted;
 }
 
 function cypherMeReverseCase(password) {
-  var passwordEnOrDecrypted = "";
+  var passwordEnOrDecrypted = '';
 
   for (var i = password.length - 1; i >= 0; i--) {
     var currentCharacter = password.charCodeAt(i);
@@ -131,8 +136,72 @@ function cypherMeReverseCase(password) {
   return passwordEnOrDecrypted;
 }
 
-function toggleView() {
+function cypherMeViginereEncrypt(passwordToEncrypt, keyword) {
+  var passwordEncryped = '';
+  var filteredKeyword = cypherMeViginereFilter(keyword);
 
+  for(var i = 0, j = 0; i < passwordToEncrypt.length; i++) {
+
+    var currentCharacter = passwordToEncrypt.charCodeAt(i);
+  
+    switch(true) {
+      case (currentCharacter >= 65 && currentCharacter <=  90):
+        passwordEncryped += String.fromCharCode( ( (currentCharacter - 65) + (filteredKeyword[j % filteredKeyword.length] - 97) ) % 26 + 65 );
+        j++;
+        break;
+      case (currentCharacter >= 97 && currentCharacter <= 122):
+        passwordEncryped += String.fromCharCode( ( (currentCharacter - 97) + (filteredKeyword[j % filteredKeyword.length] - 97) ) % 26 + 97 );
+        j++;
+        break;
+      default:
+        passwordEncryped += passwordToEncrypt.charAt(i);
+        // j++;
+    }
+  }
+
+  return passwordEncryped;
+}
+
+function cypherMeViginereDecrypt(passwordToDecrypt, keyword) {
+  var passwordDecrypted = '';
+  var filteredKeyword = cypherMeViginereFilter(keyword);
+
+  for(var i=0, j=0; i < passwordToDecrypt.length; i++) {
+
+    var currentCharacter = passwordToDecrypt.charCodeAt(i);
+  
+    switch(true) {
+      case (currentCharacter >= 65 && currentCharacter <=  90):
+        passwordDecrypted += String.fromCharCode( ( (currentCharacter - 65) - (filteredKeyword[j % filteredKeyword.length] % 26 - 97) ) % 26 + 65 );
+        j++;
+        break;
+      case (currentCharacter >= 97 && currentCharacter <= 122):
+        passwordDecrypted += String.fromCharCode( ( (currentCharacter - 97) - (filteredKeyword[j % filteredKeyword.length] % 26 - 97) ) % 26 + 97 );
+        j++;
+        break;
+      default:
+        passwordDecrypted += passwordToDecrypt.charAt(i);
+    }
+  }
+
+  return passwordDecrypted;
+}
+
+function cypherMeViginereFilter(keyword) {
+  var filteredKeyword = [];
+
+  for (var i = 0; i < keyword.length; i++) {
+    var currentCharacter = keyword.toLowerCase().charCodeAt(i);
+
+    if (currentCharacter >= 97 && currentCharacter <= 122 ) {
+      filteredKeyword.push(keyword.toLowerCase().charCodeAt(i));
+    }
+  }
+
+  return filteredKeyword;
+}
+
+function toggleView() {
   var cypherElement = document.getElementById('cypher');
   var cypher = cypherElement.options[cypherElement.selectedIndex].value;
 
@@ -140,15 +209,24 @@ function toggleView() {
     case ('caesar'): 
       document.getElementById('shift').style.display = 'inline';
       document.getElementById('shiftLabel').style.display = 'inline';
+      document.getElementById('keyword').style.display = 'none';
+      document.getElementById('keywordLabel').style.display = 'none';
+      break;
+    case ('vigenere'): 
+      document.getElementById('keyword').style.display = 'inline';
+      document.getElementById('keywordLabel').style.display = 'inline';
+      document.getElementById('shift').style.display = 'none';
+      document.getElementById('shiftLabel').style.display = 'none';
       break;
     default:
       document.getElementById('shift').style.display = 'none';
       document.getElementById('shiftLabel').style.display = 'none';
+      document.getElementById('keyword').style.display = 'none';
+      document.getElementById('keywordLabel').style.display = 'none';
   }
 }
 
 function toggleViewToDecrypt() {
-
   var cypherElement = document.getElementById('cypherToDecrypt');
   var cypherToDecrypt = cypherElement.options[cypherElement.selectedIndex].value;
 
@@ -156,10 +234,20 @@ function toggleViewToDecrypt() {
     case ('caesar'): 
       document.getElementById('shiftToDecrypt').style.display = 'inline';
       document.getElementById('shiftToDecryptLabel').style.display = 'inline';
+      document.getElementById('keywordToDecrypt').style.display = 'none';
+      document.getElementById('keywordToDecryptLabel').style.display = 'none';
+      break;
+    case ('vigenere'): 
+      document.getElementById('keywordToDecrypt').style.display = 'inline';
+      document.getElementById('keywordToDecryptLabel').style.display = 'inline';
+      document.getElementById('shiftToDecrypt').style.display = 'none';
+      document.getElementById('shiftToDecryptLabel').style.display = 'none';
       break;
     default:
       document.getElementById('shiftToDecrypt').style.display = 'none';
       document.getElementById('shiftToDecryptLabel').style.display = 'none';
+      document.getElementById('keywordToDecrypt').style.display = 'none';
+      document.getElementById('keywordToDecryptLabel').style.display = 'none';
   }
 }
 
