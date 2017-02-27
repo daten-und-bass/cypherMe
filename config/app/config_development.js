c04c856ec6cba92e6cc44756cb6f102645e7c63f
arne-at-daten-und-bass <arne@daten-und-bass.io>
2017-02-27 13:10:31 +0100

    Travis with dummy certs  in repo

--- config/app/config_development.js
+++ config/app/config_development.js
@@ -18,8 +18,8 @@ var appConfig = {
       // },
       https: {
         port: process.env.DNB_NODE_OAI_HTTPS_PORT,
-        // crt: '/etc/ssl/' + process.env.DNB_APP_ENV_S1_WB_FTED_HTTPS_CERT1,
-        // key: '/etc/ssl/' + process.env.DNB_APP_ENV_S1_WB_FTED_HTTPS_CERT1_KEY,
+        crt: '/etc/ssl/' + process.env.DNB_APP_ENV_S1_WB_FTED_HTTPS_CERT1,
+        key: '/etc/ssl/' + process.env.DNB_APP_ENV_S1_WB_FTED_HTTPS_CERT1_KEY,
       },
       // not an ip ... maybe better hops .. change nginx ip settings (show real client ip)
       // proxies: [ process.env.DNB_INF1_ENV_S1_WF ],
@@ -31,9 +31,9 @@ var appConfig = {
 
 (function readPKIFiles() {
   // process.env.WEB_HTTPS_KEY = fs.readFileSync(appConfig.web().https.key, 'utf8');
-  // process.env.CYPHERME_WEB_HTTPS_KEY = fs.readFileSync(appConfig.web().https.key, 'utf8');
+  process.env.CYPHERME_WEB_HTTPS_KEY = fs.readFileSync(appConfig.web().https.key, 'utf8');
   // process.env.WEB_HTTPS_CRT = fs.readFileSync(appConfig.web().https.crt, 'utf8');
-  // process.env.CYPHERME_WEB_HTTPS_CRT = fs.readFileSync(appConfig.web().https.crt, 'utf8');
+  process.env.CYPHERME_WEB_HTTPS_CRT = fs.readFileSync(appConfig.web().https.crt, 'utf8');
 
   // fs.unlinkSync(appConfig.web().https.key)  
 })();
