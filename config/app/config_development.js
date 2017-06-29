b6a5f65fd7bd61727584f72baee333f02535aa98
arne-at-daten-und-bass <arne@daten-und-bass.io>
2017-06-29 08:37:59 +0200

    Changed ENV VARs to new naming and rm old code

--- config/app/config_development.js
+++ config/app/config_development.js
@@ -7,20 +7,15 @@ var appConfig = {
     var environment = {
       name: process.env.NODE_ENV,
     };
+
     return environment;
   },
 
   web: function() {
     var web = {
-      // ip: process.env.DUNDB_APP_ENV_S2_WB1_IRME_IP1, // not used
-      // http: {
-      //   port: process.env.DUNDB_APP_ENV_S2_WB1_IRME_HTTP_PORT, // not used
-      // },
       https: {
         port: process.env.DNB_N_NODEJS_OAI_HTTPS_PORT,
-        // crt: '/etc/ssl/' + process.env.DNB_APP_ENV_S1_WB_FTED_HTTPS_CERT1,
         crt: process.env.DNB_ENV_V_SEC_PATH + '/' + process.env.DNB_ENV_APP_S1_WB_FTED_HTTPS_CERT1_PUB,
-        // key: '/etc/ssl/' + process.env.DNB_APP_ENV_S1_WB_FTED_HTTPS_CERT1_KEY,
         key: process.env.DNB_ENV_V_SEC_PATH + '/' + process.env.DNB_ENV_APP_S1_WB_FTED_HTTPS_CERT1_KEY,
       },
       // not an ip ... maybe better hops .. change nginx ip settings (show real client ip)
@@ -32,12 +27,8 @@ var appConfig = {
 };
 
 (function readPKIFiles() {
-  // process.env.WEB_HTTPS_KEY = fs.readFileSync(appConfig.web().https.key, 'utf8');
   process.env.CYPHERME_WEB_HTTPS_KEY = fs.readFileSync(appConfig.web().https.key, 'utf8');
-  // process.env.WEB_HTTPS_CRT = fs.readFileSync(appConfig.web().https.crt, 'utf8');
   process.env.CYPHERME_WEB_HTTPS_CRT = fs.readFileSync(appConfig.web().https.crt, 'utf8');
-
-  // fs.unlinkSync(appConfig.web().https.key)  
 })();
 
 module.exports = appConfig;
